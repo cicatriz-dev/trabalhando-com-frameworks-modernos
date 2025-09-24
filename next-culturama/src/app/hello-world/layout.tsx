b5fe69cf6b6737b706f9a4789c34bb750af1f600
Pedro Mello <contato@cicatriz.dev>
2025-09-24 19:59:17 -0300

    video 2.2

---
+++ next-culturama/src/app/hello-world/layout.tsx
@@ -0,0 +1,5 @@
+export default function HelloWorldLayout({ children }: { children: React.ReactNode }) {
+	<html lang='pt-BR'>
+		<body className='bg-red-400'>{children}</body>
+	</html>;
+}
---
+++ next-culturama/src/app/hello-world/page.tsx
@@ -0,0 +1,7 @@
+export default function HelloWorld() {
+	return (
+		<div>
+			<h1>Hello World dentro de uma pasta com App Router</h1>
+		</div>
+	);
+}
